c50612b3f0402c09e8a07b856f8e40607e168ace
RohanDSkari <rohandanny484@gmail.com>
2024-09-21 09:34:03 +0530

    testing

---
+++ A_3_14.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define IOS ios::sync_with_stdio(0);cin.tie(nullptr);cout.tie(nullptr);
+#define endl '\n'
+#define int long long
+#define pb(a) push_back(a)
+#define v vector
+#define vi v<int>
+#define bl cout<<endl;
+#define all(a) a.begin(),a.end()
+#define deb(x) cout<<#x<<" = "<<x<<endl;
+template<typename T>istream& operator>>(istream& is, v<T>& v){for(auto& x : v)is >> x;return is;}
+template<typename T>ostream& operator<<(ostream& os, v<T>& v){for(auto& x : v)os << x << ' ';return os;}
+
+void solve(){
+    int n;cin>>n;
+    string s="3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679";
+    cout<<s.substr(0,n+2);
+}
+int32_t main(){
+    IOS int t=1;
+    // cin>>t;
+    while(t--) solve();
+}
+/*
+
+*/
